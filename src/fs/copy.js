1b2e08c546bb959ed5a888285e0ef44d70951f4e
The-click <ramzanmal04@mail.ru>
2025-04-20 16:27:28 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const sourceDirPath = path.join(path.dirname("./"), "files");
+        const destinationDirPath = path.join(path.dirname("./"), "files_copy");
+
+        await fs.cp(sourceDirPath, destinationDirPath, {
+            recursive: true,
+            errorOnExist: true,
+            force: false,
+            mode: fs.constants.COPYFILE_EXCL,
+        });
+    } catch (err) {
+        if (err.code === "ERR_FS_CP_EEXIST" || err.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        }
+
+        console.log(err);
+    }
 };
 
 await copy();
