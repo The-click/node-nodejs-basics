cb2511dbae26e177d4a3453bfecf2bd480d0a57a
The-click <ramzanmal04@mail.ru>
2025-04-20 14:59:23 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(path.dirname("./"), "files", "fresh.txt");
+
+        await fs.writeFile(filePath, "I am fresh and young", {
+            flag: "wx",
+        });
+    } catch (err) {
+        if (err.code === "EEXIST") {
+            throw new Error("FS operation failed");
+        }
+
+        console.log(err);
+    }
 };
 
 await create();
