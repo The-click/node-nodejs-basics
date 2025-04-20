de3b06877fc7b7289d1ebef7efd2cab06de3b4fa
The-click <ramzanmal04@mail.ru>
2025-04-21 00:17:24 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(path.dirname("./"), "files");
+        const files = await fs.readdir(filePath);
+
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        }
+
+        console.log(err);
+    }
 };
 
 await list();
