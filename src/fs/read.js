cfee82d2672c8858341a6699e7d13df6a7818d46
The-click <ramzanmal04@mail.ru>
2025-04-21 00:28:43 +0300

    feat: add read script

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(
+            path.dirname("./"),
+            "files",
+            "fileToRead.txt"
+        );
+
+        const contents = await fs.readFile(filePath, { encoding: "utf8" });
+
+        console.log(contents);
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        }
+
+        console.log(err);
+    }
 };
 
 await read();
