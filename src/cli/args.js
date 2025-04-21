f5072fea3ed7513f3eebdb0ce72523eb8da2990b
The-click <ramzanmal04@mail.ru>
2025-04-21 21:11:56 +0300

    feat: add args func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argumentsList = [];
+    let propString = "";
+
+    process.argv.slice(2).forEach((arg) => {
+        if (arg.startsWith("--")) {
+            propString = arg;
+        } else {
+            propString += " is " + arg;
+            argumentsList.push(propString);
+        }
+    });
+
+    console.log(argumentsList.join(","));
 };
 
 parseArgs();
