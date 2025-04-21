5a592316dda2f0957108205383e11399759faefb
The-click <ramzanmal04@mail.ru>
2025-04-21 23:49:21 +0300

    feat: add calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import crypto from "crypto";
+import path from "node:path";
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const hashValue = await getHash(
+            path.join(path.dirname("./"), "files", "fileToCalculateHashFor.txt")
+        );
+        console.log(hashValue);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 };
+
+const getHash = (path) =>
+    new Promise((resolve, reject) => {
+        const hash = crypto.createHash("sha256");
+        const rs = fs.createReadStream(path);
+        rs.on("error", reject);
+        rs.on("data", (chunk) => hash.update(chunk));
+        rs.on("end", () => resolve(hash.digest("hex")));
+    });
 
 await calculateHash();
