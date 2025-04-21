670cda69098e76b71f886562c64e3de7de2cefea
The-click <ramzanmal04@mail.ru>
2025-04-21 19:56:50 +0300

    feat: add env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    let returnString = "";
+
+    Object.keys(process.env).forEach((key) => {
+        if (key.startsWith("RSS_")) {
+            returnString += `${key}=${process.env[key]}; `;
+        }
+    });
+
+    console.log(returnString);
 };
 
 parseEnv();
