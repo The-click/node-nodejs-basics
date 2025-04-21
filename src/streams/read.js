e5e5ec3045b90dfa08a1f15920694c5213ea9a6a
The-click <ramzanmal04@mail.ru>
2025-04-22 00:10:27 +0300

    feat: add function readStream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import path from "node:path";
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const rs = fs.createReadStream(
+            path.join(path.dirname("./"), "files", "fileToRead.txt")
+        );
+
+        rs.on("error", (err) => {
+            throw err;
+        });
+        rs.on("data", (chunk) => {
+            process.stdout.write(chunk + "\n");
+        });
+        rs.on("end", () => console.log("end"));
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 await read();
