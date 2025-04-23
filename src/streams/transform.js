6b845ca86318bacaf88cd5561d80fd63f2472fcc
The-click <ramzanmal04@mail.ru>
2025-04-24 01:26:45 +0300

    feat: add transfrom stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { Transform, pipeline } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+    class ReverseTransform extends Transform {
+        _transform(chunk, encoding, callback) {
+            const splitString = chunk
+                .toString()
+                .split("")
+                .slice(0, -1)
+                .reverse();
+            splitString.push("\n");
+            this.push(splitString.join(""));
+            callback();
+        }
+    }
+    const reverseTransform = new ReverseTransform();
+
+    pipeline(process.stdin, reverseTransform, process.stdout, (err) => {
+        if (err) {
+            console.error("Pipeline failed.", err);
+        } else {
+            console.log("Pipeline succeeded.");
+        }
+    });
 };
 
 await transform();
