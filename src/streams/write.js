04e89c313509e82c545fc8246b3c7e794aede47f
The-click <ramzanmal04@mail.ru>
2025-04-24 00:19:47 +0300

    feat: add write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import path from "node:path";
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const writeStream = fs.createWriteStream(
+            path.join(path.dirname("./"), "files", "fileToWrite.txt")
+        );
+
+        process.stdin.on("data", (data) => {
+            writeStream.write(data, (error) => {
+                if (error) {
+                    throw error;
+                }
+            });
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 await write();
