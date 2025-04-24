f7122e765d3312f8d2cb229239aa9291635ba4ac
The-click <ramzanmal04@mail.ru>
2025-04-24 20:02:26 +0300

    refactor: rename stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,17 +3,17 @@ import path from "node:path";
 
 const read = async () => {
     try {
-        const rs = fs.createReadStream(
+        const readStream = fs.createReadStream(
             path.join(path.dirname("./"), "files", "fileToRead.txt")
         );
 
-        rs.on("error", (err) => {
+        readStream.on("error", (err) => {
             throw err;
         });
-        rs.on("data", (chunk) => {
+        readStream.on("data", (chunk) => {
             process.stdout.write(chunk + "\n");
         });
-        rs.on("end", () => console.log("end"));
+        readStream.on("end", () => console.log("end"));
     } catch (e) {
         console.log(e);
     }
