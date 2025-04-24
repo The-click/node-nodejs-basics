94e7501ff778716dc32679914f612c3f3a3c9fff
The-click <ramzanmal04@mail.ru>
2025-04-24 20:02:44 +0300

    feat: add compress script

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { pipeline } from "node:stream/promises";
+import path from "path";
+import { createGzip } from "node:zlib";
+import fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const readStream = fs.createReadStream(
+            path.join(path.dirname("./"), "files", "fileToCompress.txt")
+        );
+        const writeStream = fs.createWriteStream(
+            path.join(path.dirname("./"), "files", "archive.gz")
+        );
+        const gzip = createGzip();
+
+        await pipeline(readStream, gzip, writeStream);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 await compress();
