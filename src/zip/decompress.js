be0f4a27b9a1a751b152c4b692ceaf8e2996ef61
The-click <ramzanmal04@mail.ru>
2025-04-24 20:03:06 +0300

    feat: add decompress script

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { pipeline } from "node:stream/promises";
+import path from "path";
+import { createUnzip } from "node:zlib";
+import fs from "fs";
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const readStream = fs.createReadStream(
+            path.join(path.dirname("./"), "files", "archive.gz")
+        );
+        const writeStream = fs.createWriteStream(
+            path.join(path.dirname("./"), "files", "fileToCompress.txt")
+        );
+        const unzip = createUnzip();
+
+        await pipeline(readStream, unzip, writeStream);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 await decompress();
