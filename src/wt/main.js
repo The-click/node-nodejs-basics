60221d394b5413dd87670ead0629608fb025476c
The-click <ramzanmal04@mail.ru>
2025-04-25 01:13:51 +0300

    feat: add worker thread script

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker } from "worker_threads";
+import os from "node:os";
+
 const performCalculations = async () => {
-    // Write your code here
+    try {
+        const availableWorkerNum = os.availableParallelism();
+        const promiseWorkers = [];
+
+        for (let numWorker = 0; availableWorkerNum > numWorker; numWorker++) {
+            promiseWorkers.push(runWorker(10 + numWorker));
+        }
+
+        const result = await Promise.all(promiseWorkers);
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
 };
+
+function runWorker(workerData) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker("./worker.js", { workerData });
+        worker.on("message", resolve);
+        worker.on("error", reject);
+        worker.on("exit", (code) => {
+            if (code !== 0)
+                reject(new Error(`Worker stopped with exit code ${code}`));
+        });
+    });
+}
 
 await performCalculations();
