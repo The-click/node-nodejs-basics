d52a84d0d43d05cd26e775b11d29bced5aad340b
The-click <ramzanmal04@mail.ru>
2025-04-26 02:36:53 +0300

    feat: add spawn childe process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,33 @@
+import { fork } from "node:child_process";
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    try {
+        const filePath = path.join(path.dirname("./"), "files", "script.js");
+        const child = fork(filePath, args, { stdio: "pipe" });
+
+        child.stdout.on("data", (data) => {
+            console.log(`stdout: ${data.slice(0, -1)}`);
+        });
+
+        child.stderr.on("data", (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        child.on("close", (code) => {
+            console.log(`Дочерний процесс завершился с кодом ${code}`);
+
+            if (code != 0) {
+                throw new Error("Дочерний процесс завершился с ошибкой");
+            }
+        });
+
+        process.stdin.on("data", (data) => {
+            child.stdin.write(data);
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
